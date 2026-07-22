27ec011265dbbc582787fa65a51929e48924407e
khalil-mejri-1 <mejrik1888@gmail.com>
2026-07-22 14:05:40 +0100

    final update

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
 // export const API_BASE_URL = "http://localhost:3000";
-export const API_BASE_URL = "https://stitch-d-barras.vercel.app";
+// export const API_BASE_URL = "https://stitch-d-barras.vercel.app";
+export const API_BASE_URL = "https://stitch-debarras.com";
